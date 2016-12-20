bcd4c4a7c61a824676cd175cce0a6ca44f11b09d
强哥的mac <979927262@qq.com>
2016-12-20 14:57:41 +0800

    轮播图 实体类

--- CEM/app/src/main/java/com/example/qianggedemac/cem/film/hot/HotFragment.java
+++ CEM/app/src/main/java/com/example/qianggedemac/cem/film/hot/HotFragment.java
@@ -7,15 +7,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.qianggedemac.cem.R;
 import com.example.qianggedemac.cem.baseclass.BaseFragment;
+import com.example.qianggedemac.cem.tool.url.UrlTools;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HotFragment extends BaseFragment {
 
-
+    private HotFragmentBean mHotFragmentBean;
 
     @Override
     protected int setLayout() {
@@ -29,6 +35,34 @@ public class HotFragment extends BaseFragment {
 
     @Override
     protected void initData() {
+        /**
+         * 轮播图数据
+         */
+     parseBanner();
+        /**
+         * listView数据
+         */
+        parseListView();
+
+    }
+
+    private void parseListView() {
+        RequestQueue requestQueue = Volley.newRequestQueue(mContext);
+        StringRequest stringRequest = new StringRequest(UrlTools.TURN_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(stringRequest);
+    }
+
+    private void parseBanner() {
 
     }
 
--- CEM/app/src/main/java/com/example/qianggedemac/cem/tool/url/UrlTools.java
+++ CEM/app/src/main/java/com/example/qianggedemac/cem/tool/url/UrlTools.java
@@ -6,4 +6,6 @@ package com.example.qianggedemac.cem.tool.url;
 
 public class UrlTools {
     public static final String WELCOME_URL = "http://api.maoyan.com/apptools/launchScreen.json?city_id=65&poster_type=1&utm_campaign=AmovieBmovieCD100&movieBundleVersion=7601&utm_source=xiaomi&utm_medium=android&utm_term=7.6.0&utm_content=860887036129012&ci=65&net=255";
+    //轮播图
+    public static final String TURN_URL = "http://advert.mobile.meituan.com/api/v3/adverts?cityid=65&category=11&version=7.6.0&new=0&app=movie&clienttp=android&uuid=89B38BD34B3C4B2B83BA7D389BEB138BD05D8DE7D11FD20C6BAAC72F1870761D&devid=860887036129012&uid=&movieid=&partner=1&apptype=1&smId=&utm_campaign=AmovieBmovieCD-1";
 }
