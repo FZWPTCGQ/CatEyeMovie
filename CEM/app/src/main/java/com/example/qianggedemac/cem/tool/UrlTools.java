7e729daeae396d56afbf37e5742ef0d30d832366
强哥的mac <979927262@qq.com>
2016-12-21 16:50:20 +0800

    添加网址

--- CEM/app/src/main/java/com/example/qianggedemac/cem/tool/UrlTools.java
+++ CEM/app/src/main/java/com/example/qianggedemac/cem/tool/UrlTools.java
@@ -11,5 +11,9 @@ public class UrlTools {
     public static final String TURN_URL = "http://advert.mobile.meituan.com/api/v3/adverts?cityid=65&category=11&version=7.6.0&new=0&app=movie&clienttp=android&uuid=89B38BD34B3C4B2B83BA7D389BEB138BD05D8DE7D11FD20C6BAAC72F1870761D&devid=860887036129012&uid=&movieid=&partner=1&apptype=1&smId=&utm_campaign=AmovieBmovieCD-1";
     //首页热映页面listView
     public static final String HOT_URL = "http://api.maoyan.com/mmdb/movie/v3/list/hot.json?ci=65&limit=12&token=&utm_campaign=AmovieBmovieCD-1&movieBundleVersion=7601&utm_source=vivo_dy&utm_medium=android&utm_term=7.6.0&utm_content=861469039886796&net=255";
+    // 发现 -> 今天
+    public static final String FIND_TODAY = "http://api.maoyan.com/sns/v5/feed.json?";
+    //发现 -> 上面四个图标
+    public static final String FIND_TOP = "http://api.maoyan.com/sns/v2/buttons.json?utm_term=7.5.0&utm_medium=android";
 
 }
