275502e1a11b5b05a4557f3bf1d3e22b6dd2aa04
强哥的mac <979927262@qq.com>
2016-12-26 19:09:38 +0800

    改变waitFragment

--- CEM/app/src/main/java/com/example/qianggedemac/cem/film/wait/WaitFragment.java
+++ CEM/app/src/main/java/com/example/qianggedemac/cem/film/wait/WaitFragment.java
@@ -23,63 +23,77 @@ import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
  * A simple {@link Fragment} subclass.
  */
 public class WaitFragment extends BaseFragment {
-
-
-    private StickyListHeadersListView mStickyListHeadersListView;
-    private WaitFragmentAdapter mWaitFragmentAdapter;
-
     @Override
     protected int setLayout() {
-        return R.layout.fragment_wait;
+        return 0;
     }
 
     @Override
     protected void initView(View view) {
-        mStickyListHeadersListView = (StickyListHeadersListView)view.findViewById(R.id.fragment_wait_stickyListHeaderListView);
-        mWaitFragmentAdapter = new WaitFragmentAdapter(mContext);
 
     }
 
     @Override
     protected void initData() {
-     parseInternet();
-        /**
-         * 刷新
-         */
-      refreshMethod();
-    }
-
-    private void refreshMethod() {
 
     }
 
-    private void parseInternet() {
-        OkHttpManager.getInstance().get(UrlTools.MOVIE_WAIT_LISTVIEW, NearBean1.class, new NetCallBack<NearBean1>() {
-            @Override
-            public void onResponse(NearBean1 bean) {
-                mWaitFragmentAdapter.setNearBean(bean);
-                List<String> list = new ArrayList<String>();
-                Map<String,List<NearBean1.DataBean.ComingBean>> map = new ArrayMap<String, List<NearBean1.DataBean.ComingBean>>();
-                for (int i = 0; i < bean.getData().getComing().size(); i++) {
-                    String date = bean.getData().getComing().get(i).getComingTitle();
-                    list.add(date);
-                    map.put(date,bean.getData().getComing());
-                    Log.d("阿范德萨发大水", "map.size():" + map.size());
-                }
 
-                mWaitFragmentAdapter.setLists(list);
-                mWaitFragmentAdapter.setMap(map);
-                mStickyListHeadersListView.setAdapter(mWaitFragmentAdapter);
-
-            }
-
-            @Override
-            public void onError(Exception e) {
-
-            }
-        });
-        View view = LayoutInflater.from(mContext).inflate(R.layout.search_layout,null);
-        mStickyListHeadersListView.addHeaderView(view);
-    }
+//    private StickyListHeadersListView mStickyListHeadersListView;
+//    private WaitFragmentAdapter mWaitFragmentAdapter;
+//
+//    @Override
+//    protected int setLayout() {
+//        return R.layout.fragment_wait;
+//    }
+//
+//    @Override
+//    protected void initView(View view) {
+//        mStickyListHeadersListView = (StickyListHeadersListView)view.findViewById(R.id.fragment_wait_stickyListHeaderListView);
+//        mWaitFragmentAdapter = new WaitFragmentAdapter(mContext);
+//
+//    }
+//
+//    @Override
+//    protected void initData() {
+//     parseInternet();
+//        /**
+//         * 刷新
+//         */
+//      refreshMethod();
+//    }
+//
+//    private void refreshMethod() {
+//
+//    }
+//
+//    private void parseInternet() {
+//        OkHttpManager.getInstance().get(UrlTools.MOVIE_WAIT_LISTVIEW, NearBean1.class, new NetCallBack<NearBean1>() {
+//            @Override
+//            public void onResponse(NearBean1 bean) {
+//                mWaitFragmentAdapter.setNearBean(bean);
+//                List<String> list = new ArrayList<String>();
+//                Map<String,List<NearBean1.DataBean.ComingBean>> map = new ArrayMap<String, List<NearBean1.DataBean.ComingBean>>();
+//                for (int i = 0; i < bean.getData().getComing().size(); i++) {
+//                    String date = bean.getData().getComing().get(i).getComingTitle();
+//                    list.add(date);
+//                    map.put(date,bean.getData().getComing());
+//                    Log.d("阿范德萨发大水", "map.size():" + map.size());
+//                }
+//
+//                mWaitFragmentAdapter.setLists(list);
+//                mWaitFragmentAdapter.setMap(map);
+//                mStickyListHeadersListView.setAdapter(mWaitFragmentAdapter);
+//
+//            }
+//
+//            @Override
+//            public void onError(Exception e) {
+//
+//            }
+//        });
+//        View view = LayoutInflater.from(mContext).inflate(R.layout.search_layout,null);
+//        mStickyListHeadersListView.addHeaderView(view);
+//    }
 
 }
